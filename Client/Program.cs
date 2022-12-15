4435bc106076025b3ff322ed912decc61393cd02
Ali-Roodi79 <ali.roodi1379@gmail.com>
2022-12-15 22:41:42 +0330

    Upload Sockeet programming asynchoronous Project

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace client
+{
+    class Program
+    {
+        static byte[] clientPayam2 = new byte[1024];
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress[] AllIP = Dns.GetHostAddresses("127.0.0.1");
+                IPEndPoint serverEndPoint = new IPEndPoint(AllIP[0], 9999);
+                client.BeginConnect(serverEndPoint, connectcall, client);
+                Console.WriteLine("Client connecting to Server");
+                Console.WriteLine("Type your Message");
+                string Message = Console.ReadLine();
+                byte[] ClientPayam = Encoding.UTF8.GetBytes(Message);
+                client.BeginSend(ClientPayam, 0, ClientPayam.Length, SocketFlags.None, sendcall, client);
+                void connectcall(IAsyncResult ar)
+                {
+                    Socket Client = (Socket)ar.AsyncState;
+                    Client.EndConnect(ar);
+                }
+                client.BeginReceive(clientPayam2, 0, clientPayam2.Length, SocketFlags.None, receievecall, client);
+                void receievecall(IAsyncResult ar)
+                {
+
+                    Socket Client = (Socket)ar.AsyncState;
+                    int clientRecived = Client.EndReceive(ar);
+                    string Data = Encoding.UTF8.GetString(clientPayam2, 0, clientRecived);
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Client Recive \"" + clientRecived + "\" byte From Server");
+                    Console.WriteLine("Client Recive \"" + Data + "\" From Server");
+                    Console.WriteLine("-----------------------------");
+                }
+                void sendcall(IAsyncResult ar)
+                {
+                    Socket Client = (Socket)ar.AsyncState;
+                    int ClientSent = Client.EndSend(ar);
+                    Console.WriteLine("Client Send \"" + Message + "\" To Server");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static byte[] ServerPayam = new byte[1024];
+        static byte[] resend = new byte[1024];
+        static string Data;
+        static Socket AcceptedServer;
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress[] AllIP = Dns.GetHostAddresses("127.0.0.1");
+            IPEndPoint serverEndPoint = new IPEndPoint(AllIP[0], 9999);
+            server.Bind(serverEndPoint);
+            server.Listen(100);
+            while (true)
+            {
+                server.BeginAccept(AcceptCallback, server);
+                void AcceptCallback(IAsyncResult ar)
+                {
+                    Socket Server = (Socket)ar.AsyncState;
+                    Socket AcceptedServer = server.EndAccept(ar);
+                    Console.WriteLine("server Accepted Connection");
+                    server.BeginAccept(AcceptCallback, server);
+                    AcceptedServer.BeginReceive(ServerPayam, 0, ServerPayam.Length, SocketFlags.None, ReceiveCallback, AcceptedServer);
+                }
+
+                void ReceiveCallback(IAsyncResult ar)
+                {
+                    AcceptedServer = (Socket)ar.AsyncState;
+                    int ServerRecived = AcceptedServer.EndReceive(ar);
+                    Data = Encoding.UTF8.GetString(ServerPayam, 0, ServerRecived);
+                    Console.WriteLine("Server Recive \"" + ServerRecived + "\" byte From Client");
+                    Console.WriteLine("Server Recive \"" + Data + "\" From Client");
+                    Resend();
+                    AcceptedServer.BeginReceive(ServerPayam, 0, ServerPayam.Length, SocketFlags.None, ReceiveCallback, AcceptedServer);
+                }
+                void Resend()
+                {
+                    resend = Encoding.UTF8.GetBytes(Data);
+                    AcceptedServer.Send(resend, SocketFlags.None);
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Server send \"" + Data + "\" to client");
+                    Console.WriteLine("-----------------------------");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
